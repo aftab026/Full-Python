8346143228da7495216f95de998b88ff0565c6b7
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:09:13 +0530

    Add files via upload

---
+++ 07_typefunction _typecasting.py
@@ -0,0 +1,18 @@
+#type() function
+
+a = 3
+
+print(type(a))
+#print(a)
+
+b = "23" # string also written as '23' and '''23'''
+print(type(b))
+
+#Typecasting
+a = 31
+a = str(31)
+print(type(a))
+
+b = '55'
+b = int(55)
+print(type(b))
