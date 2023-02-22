3fb839d071eb216d328488c2fc39038a08b2f437
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:15:16 +0530

    Add files via upload

---
+++ 61_continue.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    
+    #if i %2 == 0: # print odd numbere
+    #if i %2 != 0: # print even numbere
+    if i == 5:
+        continue
+    print(i)
+
+    
---
+++ 62_pass_statement.py
@@ -0,0 +1,16 @@
+i = 4
+
+
+def run(player):
+    pass
+
+def ouch(player):
+    pass
+
+if i> 0:
+    pass
+print("Harry is a good boy")
+
+
+
+
