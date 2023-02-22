15b64de7dff933d74066ef618eb7f0a44a0fb952
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:08:37 +0530

    Add files via upload

---
+++ 05_variable.py
@@ -0,0 +1,22 @@
+a = '''Aftab'''
+# a = 'Aftab'
+# a = "Aftab"
+b = 44
+c = 77.7
+d = False
+e = None
+
+# printing the variable
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
