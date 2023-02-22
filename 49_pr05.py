cef10652de284ddd50a409fab8fcefeb2becbd14
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:14:07 +0530

    Add files via upload

---
+++ 49_pr05.py
@@ -0,0 +1,6 @@
+name = input("Enter your name\n")
+if len(name)< 10:
+    print("TRUE")
+else:
+    print("FALSE")
+
---
+++ 50_pr06.py
@@ -0,0 +1,7 @@
+names = ["harry", "rohit", "dhoni", "virat", "siraj", "kohli", "hardik", "dhawan"]
+name = input("Enter the name to check \n")
+
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
