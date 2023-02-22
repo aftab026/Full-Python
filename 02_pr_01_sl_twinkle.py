5dfaf69494449e0b5583b635ba0434cbdf756fef
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:03:05 +0530

    Add files via upload

---
+++ 02_pr_01_sl_twinkle.py
@@ -0,0 +1,13 @@
+'''
+This problem is a solution of 
+problem 1 of harry practice set!
+'''
+# This is also a comment just like the above line
+
+
+
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
