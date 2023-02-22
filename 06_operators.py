bcf34c92972e61fbb2580201841755e1e21a90e0
Aftab <110347957+aftab026@users.noreply.github.com>
2023-02-22 06:08:57 +0530

    Add files via upload

---
+++ 06_operators.py
@@ -0,0 +1,40 @@
+# Arithmetic operators
+a = input("Enter the first value:\n")
+b = input("Enter the second value\n")
+a = int(a)
+b = int(b)
+print("The value of a+b is", a+b)   #we have to also write the value of a and b in print function and then print it.
+print("The value of a-b is", a-b)
+print("The value of a*b is", a*b)
+print("The value of a/b is", a/b)
+print("\n")
+
+# Assingnment operators
+a = 45
+# a +=4
+# a -=10
+#a *= 2
+a /= 5
+print(a)
+
+print("\n")
+
+#Coparison operator
+print(4>5)
+print(4<5)
+print(4>=5)
+print(4<=5)
+print(4==5)
+print(4!=5)
+
+
+print(("\n"))
+
+
+#Logical operators
+bool1 = True
+bool2 = True
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 or bool2 is",(bool1 or bool2))
+print("The value of  bool2 is",(not bool2))
+
